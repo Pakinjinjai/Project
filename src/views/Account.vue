d20e83d033b163a64160c98406b08c386f57fd15
PAKIN JINJAI <63201161@kmitl.ac.th>
2023-10-14 14:11:34 +0700

    Update Account

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -98,25 +98,25 @@
               <div class="grid md:grid-cols-2 text-sm">
                 <div class="grid grid-cols-2">
                   <div class="px-4 py-2 font-semibold">First Name</div>
-                  <div class="px-4 py-2">{{ formData.firstname }}</div>
+                  <div class="px-4 py-2">ภาคิน</div>
                 </div>
                 <div class="grid grid-cols-2">
                   <div class="px-4 py-2 font-semibold">Last Name</div>
-                  <div class="px-4 py-2">Doe</div>
+                  <div class="px-4 py-2">จิ้นจ้าย</div>
                 </div>
                 <div class="grid grid-cols-2">
                   <div class="px-4 py-2 font-semibold">Gender</div>
-                  <div class="px-4 py-2">Female</div>
+                  <div class="px-4 py-2">ชาย</div>
                 </div>
                 <div class="grid grid-cols-2">
                   <div class="px-4 py-2 font-semibold">Phone number</div>
-                  <div class="px-4 py-2">+11 998001001</div>
+                  <div class="px-4 py-2">0652358039</div>
                 </div>
                 <div class="grid grid-cols-2">
                   <div class="px-4 py-2 font-semibold">Email.</div>
                   <div class="px-4 py-2">
                     <a class="text-blue-800" href="mailto:jane@example.com"
-                      >jane@example.com</a
+                      >63201161@kmitl.ac.th</a
                     >
                   </div>
                 </div>
@@ -126,58 +126,51 @@
                 </div>
               </div>
             </div>
-            <div class="flex items-center space-x-2 font-semibold text-gray-900 leading-8">
-                        <span clas="text-green-500">
-                            <svg class="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
-                            </svg>
-                        </span>
-                        <span class="tracking-wide">Address</span>
+            <div
+              class="flex items-center space-x-2 font-semibold text-gray-900 leading-8"
+            >
+              <span clas="text-green-500">
+                <svg
+                  class="h-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                  />
+                </svg>
+              </span>
+              <span class="tracking-wide">Address</span>
             </div>
             <div class="text-gray-700">
               <div class="grid md:grid-cols-2 text-sm">
                 <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">First Name</div>
-                  <div class="px-4 py-2">{{ formData.firstname }}</div>
+                  <div class="px-4 py-2 font-semibold">Province:</div>
+                  <div class="px-4 py-2">นครศรีธรรมราช</div>
                 </div>
                 <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Last Name</div>
-                  <div class="px-4 py-2">Doe</div>
+                  <div class="px-4 py-2 font-semibold">District:</div>
+                  <div class="px-4 py-2">เมื่องนครศรีธรรมราช</div>
                 </div>
                 <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Gender</div>
-                  <div class="px-4 py-2">Female</div>
+                  <div class="px-4 py-2 font-semibold">Subdistrict:</div>
+                  <div class="px-4 py-2">โพธิ์เสด็จ</div>
                 </div>
                 <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Contact No.</div>
-                  <div class="px-4 py-2">+11 998001001</div>
+                  <div class="px-4 py-2 font-semibold">House No:</div>
+                  <div class="px-4 py-2">86/29</div>
                 </div>
                 <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Current Address</div>
-                  <div class="px-4 py-2">Beech Creek, PA, Pennsylvania</div>
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Permanant Address</div>
-                  <div class="px-4 py-2">Arlington Heights, IL, Illinois</div>
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Email.</div>
-                  <div class="px-4 py-2">
-                    <a class="text-blue-800" href="mailto:jane@example.com"
-                      >jane@example.com</a
-                    >
-                  </div>
-                </div>
-                <div class="grid grid-cols-2">
-                  <div class="px-4 py-2 font-semibold">Birthday</div>
-                  <div class="px-4 py-2">Feb 06, 1998</div>
+                  <div class="px-4 py-2 font-semibold">Zip code</div>
+                  <div class="px-4 py-2">80000</div>
                 </div>
               </div>
             </div>
-
-                  
           </div>
           <!-- End of about section -->
 
