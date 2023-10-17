12ff0c2f84c0a304d56d02918448c3fc8d48a4e4
PAKIN JINJAI <63201161@kmitl.ac.th>
2023-10-17 09:27:24 +0700

    add button

--- src/views/AccountApp/Account-edit.vue
+++ src/views/AccountApp/Account-edit.vue
@@ -82,7 +82,7 @@
               type="button"
               class="mt-7 text-white bg-green-700 hover:bg-green-600 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
             >
-              Done
+              Save
             </button>
             <router-link to="/account-info">
               <button
@@ -423,8 +423,7 @@ import axios from "axios";
 export default {
   name: "editAccount",
   components: {},
-  props: {
-  },
+  props: {},
   data() {
     return {
       formData: {},
