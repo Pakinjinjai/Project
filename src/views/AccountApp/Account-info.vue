5082087dc81b0fc37f8348514fc1a9c51067b33f
PAKIN JINJAI <63201161@kmitl.ac.th>
2023-10-20 02:27:25 +0700

    Update Account-info.vue

--- src/views/AccountApp/Account-info.vue
+++ src/views/AccountApp/Account-info.vue
@@ -323,29 +323,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-.account {
-  display: grid;
-  max-width: 960px;
-  margin: 0 auto;
-  border-radius: 24px;
-  background-color: #64ccc5;
-  height: 50vh;
-  width: 100vh;
-  .account-text {
-    margin-top: 15px;
-    margin-left: 15px;
-    background: black;
-    height: 50px;
-    width: 200px;
-    border-radius: 16px;
-    // text-align: center;
-    .account-edit {
-      margin-top: 25px;
-    }
-    h1 {
-      color: white;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
