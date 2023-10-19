d996fe09833d94847c46ad4d5b75efd15f277dc5
PAKIN JINJAI <63201161@kmitl.ac.th>
2023-10-19 12:02:36 +0700

    Update ContactView.vue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,6 +1,94 @@
 <template>
   <div class="page-container">
-    <div class="warpper">Contact Page</div>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead
+          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3">Name</th>
+            <th scope="col" class="px-6 py-3">Date</th>
+            <th scope="col" class="px-6 py-3">Description</th>
+            <th scope="col" class="px-6 py-3">Status</th>
+            <th scope="col" class="px-6 py-3">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Apple MacBook Pro 17"
+            </th>
+            <td class="px-6 py-4">Silver</td>
+            <td class="px-6 py-4">Laptop</td>
+            <td class="px-6 py-4">
+              <div class="flex items-center">
+                <div class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
+                Online
+              </div>
+            </td>
+            <td class="px-6 py-4">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >Edit</a
+              >
+            </td>
+          </tr>
+          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Microsoft Surface Pro
+            </th>
+            <td class="px-6 py-4">White</td>
+            <td class="px-6 py-4">Laptop PC</td>
+            <td class="px-6 py-4">
+              <div class="flex items-center">
+                <div class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
+                Online
+              </div>
+            </td>
+            <td class="px-6 py-4">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >Edit</a
+              >
+            </td>
+          </tr>
+          <tr class="bg-white dark:bg-gray-800">
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Magic Mouse 2
+            </th>
+            <td class="px-6 py-4">Black</td>
+            <td class="px-6 py-4">Accessories</td>
+            <td class="px-6 py-4">
+              <div class="flex items-center">
+                <div class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
+                <div class="flex p-3">
+                  <div>Online</div>
+                  <div>Offline</div>
+                </div>
+              </div>
+            </td>
+            <td class="px-6 py-4">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >Edit</a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -11,10 +99,7 @@ export default {};
 <style lang="scss" scoped>
 @import "@/style/main.scss";
 .page-container {
-  display: grid;
   width: 1440px;
-  height: 100vh;
-  background-color: bisque;
   position: relative;
   margin: 0 auto;
   transition: all 0.3s;
