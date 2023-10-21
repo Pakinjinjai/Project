efa90955026eab9b8398c9c9e3f3583dbd67bd6a
PAKIN JINJAI <63201161@kmitl.ac.th>
2023-10-21 22:22:01 +0700

    Install package
    npm i --save @fortawesome/fontawesome-svg-core
    # Free icons styles
    npm i --save @fortawesome/free-solid-svg-icons
    npm i --save @fortawesome/free-regular-svg-icons
    npm i --save @fortawesome/free-brands-svg-icons
    # for Vue 3.x
    npm i --save @fortawesome/vue-fontawesome@latest-3

--- src/main.js
+++ src/main.js
@@ -3,5 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import "./style.css";
 import "devextreme/dist/css/dx.light.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+library.add(faUserSecret);
 
-createApp(App).use(router).mount("#app");
+createApp(App)
+  .component("font-awesome-icon", FontAwesomeIcon)
+  .use(router)
+  .mount("#app");
