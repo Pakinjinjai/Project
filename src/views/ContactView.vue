56163d067ddd422c7444cac37efad8bca4914405
PAKIN JINJAI <63201161@kmitl.ac.th>
2023-10-21 22:22:17 +0700

    Test loop Queue

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -8,12 +8,11 @@
           <tr>
             <th scope="col" class="px-6 py-3">Name</th>
             <th scope="col" class="px-6 py-3">Date</th>
-            <th scope="col" class="px-6 py-3">Description</th>
             <th scope="col" class="px-6 py-3">Status</th>
             <th scope="col" class="px-6 py-3">Action</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody v-for="(item, index) in items" v-bind:key="index">
           <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
             <th
               scope="row"
@@ -21,60 +20,13 @@
             >
               Apple MacBook Pro 17"
             </th>
-            <td class="px-6 py-4">Silver</td>
-            <td class="px-6 py-4">Laptop</td>
+            <td class="px-6 py-4">{{ item.dateQueue }}</td>
             <td class="px-6 py-4">
               <div class="flex items-center">
-                <div class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
-                Online
-              </div>
-            </td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >Edit</a
-              >
-            </td>
-          </tr>
-          <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Microsoft Surface Pro
-            </th>
-            <td class="px-6 py-4">White</td>
-            <td class="px-6 py-4">Laptop PC</td>
-            <td class="px-6 py-4">
-              <div class="flex items-center">
-                <div class="h-2.5 w-2.5 rounded-full bg-green-500 mr-2"></div>
-                Online
-              </div>
-            </td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >Edit</a
-              >
-            </td>
-          </tr>
-          <tr class="bg-white dark:bg-gray-800">
-            <th
-              scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-            >
-              Magic Mouse 2
-            </th>
-            <td class="px-6 py-4">Black</td>
-            <td class="px-6 py-4">Accessories</td>
-            <td class="px-6 py-4">
-              <div class="flex items-center">
-                <div class="h-2.5 w-2.5 rounded-full bg-green-600 mr-2"></div>
+                <div class="h-2.5 w-2.5 rounded-full bg-green-700 mr-2"></div>
                 <div class="h-2.5 w-2.5 rounded-full bg-red-700 mr-2"></div>
                 <div class="flex p-3">
-                  <div class="text-green-600">Online</div>
+                  <div class="text-green-700">Online</div>
                   <div class="text-red-700">Offline</div>
                 </div>
               </div>
@@ -83,8 +35,16 @@
               <a
                 href="#"
                 class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                >Edit</a
-              >
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"
+                  />
+                </svg>
+              </a>
             </td>
           </tr>
         </tbody>
@@ -94,7 +54,26 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      items: [
+        {
+          dateQueue: "2023-10-19 00:00:00",
+          status: "true",
+        },
+        {
+          dateQueue: "2023-10-20 00:00:00",
+          status: "true",
+        },
+        {
+          dateQueue: "2023-10-21 00:00:00",
+          status: "true",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
