2042bcfc30126b26beb5fbae216d88be76295d5e
Satapat123 <63201171@kmitl.ac.th>
2023-12-15 15:55:25 +0700

    fix row and column

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -34,7 +34,7 @@
                     block-progression: tb;
                     marker: none;
                     -inkscape-font-specification: Sans;
-                                    "></path>
+                  "></path>
 							</svg></span>
 						<h3>ความดันโลหิต</h3>
 						<p>
@@ -118,12 +118,13 @@
 				</div>
 			</section>
 
-			<div class="row">
-				<div class="col-6 col-12-narrower">
-					<section class="box special">
-						<span class="image featured"><img src="../assets/pugun.jpeg" alt="../assets/pugun.jpeg" /></span>
-						<h3>นาย ภาคิน จิ้นจ้าย</h3>
-						<p>
+			<div class="flex flex-wrap -mx-4">
+				<div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+					<section class="box special p-4">
+						<span class="image featured w-full block mb-4"><img src="../assets/pugun.jpeg"
+								alt="../assets/doctorwo.jpeg" /></span>
+						<h3 class="text-lg font-semibold mb-2">นาย ศตพรรษ จำปา</h3>
+						<p class="text-sm text-gray-600 mb-4">
 							Integer volutpat ante et accumsan commophasellus sed aliquam
 							feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan
 							dolore magna aliquam veroeros.
@@ -133,11 +134,72 @@
 						</ul>
 					</section>
 				</div>
-				<div class="col-6 col-12-narrower">
-					<section class="box special">
-						<span class="image featured"><img src="../assets/pugun.jpeg" alt="../assets/doctorwo.jpeg" /></span>
-						<h3>นาย ศตพรรษ จำปา</h3>
-						<p>
+				<div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+					<section class="box special p-4">
+						<span class="image featured w-full block mb-4"><img src="../assets/pugun.jpeg"
+								alt="../assets/doctorwo.jpeg" /></span>
+						<h3 class="text-lg font-semibold mb-2">นาย ศตพรรษ จำปา</h3>
+						<p class="text-sm text-gray-600 mb-4">
+							Integer volutpat ante et accumsan commophasellus sed aliquam
+							feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan
+							dolore magna aliquam veroeros.
+						</p>
+						<ul class="actions special">
+							<li><a href="#" class="button alt">Learn More</a></li>
+						</ul>
+					</section>
+				</div>
+				<div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+					<section class="box special p-4">
+						<span class="image featured w-full block mb-4"><img src="../assets/pugun.jpeg"
+								alt="../assets/doctorwo.jpeg" /></span>
+						<h3 class="text-lg font-semibold mb-2">นาย ศตพรรษ จำปา</h3>
+						<p class="text-sm text-gray-600 mb-4">
+							Integer volutpat ante et accumsan commophasellus sed aliquam
+							feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan
+							dolore magna aliquam veroeros.
+						</p>
+						<ul class="actions special">
+							<li><a href="#" class="button alt">Learn More</a></li>
+						</ul>
+					</section>
+				</div>
+				<div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+					<section class="box special p-4">
+						<span class="image featured w-full block mb-4"><img src="../assets/pugun.jpeg"
+								alt="../assets/doctorwo.jpeg" /></span>
+						<h3 class="text-lg font-semibold mb-2">นาย ศตพรรษ จำปา</h3>
+						<p class="text-sm text-gray-600 mb-4">
+							Integer volutpat ante et accumsan commophasellus sed aliquam
+							feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan
+							dolore magna aliquam veroeros.
+						</p>
+						<ul class="actions special">
+							<li><a href="#" class="button alt">Learn More</a></li>
+						</ul>
+					</section>
+				</div>
+				<div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+					<section class="box special p-4">
+						<span class="image featured w-full block mb-4"><img src="../assets/pugun.jpeg"
+								alt="../assets/doctorwo.jpeg" /></span>
+						<h3 class="text-lg font-semibold mb-2">นาย ศตพรรษ จำปา</h3>
+						<p class="text-sm text-gray-600 mb-4">
+							Integer volutpat ante et accumsan commophasellus sed aliquam
+							feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan
+							dolore magna aliquam veroeros.
+						</p>
+						<ul class="actions special">
+							<li><a href="#" class="button alt">Learn More</a></li>
+						</ul>
+					</section>
+				</div>
+				<div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
+					<section class="box special p-4">
+						<span class="image featured w-full block mb-4"><img src="../assets/pugun.jpeg"
+								alt="../assets/doctorwo.jpeg" /></span>
+						<h3 class="text-lg font-semibold mb-2">นาย ศตพรรษ จำปา</h3>
+						<p class="text-sm text-gray-600 mb-4">
 							Integer volutpat ante et accumsan commophasellus sed aliquam
 							feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan
 							dolore magna aliquam veroeros.
@@ -162,342 +224,343 @@ export default {
 
 <style>
 .box {
-		background: #fff;
-		border-radius: 6px;
-		box-shadow: 0 2px 0 0 #e5e5e5;
-		margin: 0 0 2em 0;
-		padding: 3em;
-	}
+	background: #fff;
+	border-radius: 6px;
+	box-shadow: 0 2px 0 0 #e5e5e5;
+	margin: 0 0 2em 0;
+	padding: 3em;
+}
 
-		.box > :last-child {
-			margin-bottom: 0;
-		}
+.box> :last-child {
+	margin-bottom: 0;
+}
 
-		.box.alt {
-			background: none !important;
-			border-radius: 0 !important;
-			box-shadow: none !important;
-			margin: 0 0 2em 0;
-			padding: 0 !important;
-		}
+.box.alt {
+	background: none !important;
+	border-radius: 0 !important;
+	box-shadow: none !important;
+	margin: 0 0 2em 0;
+	padding: 0 !important;
+}
 
-		.box.features .features-row {
-			border-top: solid 2px #e5e5e5;
-			position: relative;
-		}
+.box.features .features-row {
+	border-top: solid 2px #e5e5e5;
+	position: relative;
+}
 
-			.box.features .features-row:after {
-				clear: both;
-				content: '';
-				display: block;
-			}
+.box.features .features-row:after {
+	clear: both;
+	content: "";
+	display: block;
+}
 
-			.box.features .features-row section {
-				float: left;
-				padding: 3em;
-				width: 50%;
-			}
+.box.features .features-row section {
+	float: left;
+	padding: 3em;
+	width: 50%;
+}
 
-				.box.features .features-row section :last-child {
-					margin-bottom: 0;
-				}
+.box.features .features-row section :last-child {
+	margin-bottom: 0;
+}
 
-				.box.features .features-row section:nth-child(2n) {
-					padding-right: 0;
-				}
+.box.features .features-row section:nth-child(2n) {
+	padding-right: 0;
+}
 
-					.box.features .features-row section:nth-child(2n):before {
-						background: #e5e5e5;
-						content: '';
-						display: block;
-						height: 100%;
-						margin-left: -3em;
-						position: absolute;
-						top: 0;
-						width: 2px;
-					}
+.box.features .features-row section:nth-child(2n):before {
+	background: #e5e5e5;
+	content: "";
+	display: block;
+	height: 100%;
+	margin-left: -3em;
+	position: absolute;
+	top: 0;
+	width: 2px;
+}
 
-				.box.features .features-row section:nth-child(2n-1) {
-					padding-left: 0;
-				}
+.box.features .features-row section:nth-child(2n-1) {
+	padding-left: 0;
+}
 
-			.box.features .features-row:first-child {
-				border-top: 0;
-			}
+.box.features .features-row:first-child {
+	border-top: 0;
+}
 
-				.box.features .features-row:first-child section {
-					padding-top: 0;
-				}
+.box.features .features-row:first-child section {
+	padding-top: 0;
+}
 
-			.box.features .features-row:last-child {
-				padding-bottom: 0;
-			}
+.box.features .features-row:last-child {
+	padding-bottom: 0;
+}
 
-				.box.features .features-row:last-child section {
-					padding-bottom: 0;
-				}
+.box.features .features-row:last-child section {
+	padding-bottom: 0;
+}
 
-		.box.special {
-			text-align: center;
-		}
+.box.special {
+	text-align: center;
+}
 
-		.box .image.featured {
-			border-radius: 0;
-			display: block;
-			margin: 3em 0 3em -3em;
-			position: relative;
-			width: calc(100% + 6em);
-		}
+.box .image.featured {
+	border-radius: 0;
+	display: block;
+	margin: 3em 0 3em -3em;
+	position: relative;
+	width: calc(100% + 6em);
+}
 
-			.box .image.featured img {
-				border-radius: 0;
-				display: block;
-				width: 100%;
-			}
+.box .image.featured img {
+	border-radius: 0;
+	display: block;
+	width: 100%;
+}
 
-			.box .image.featured:first-child {
-				border-radius: 6px 6px 0 0;
-				margin-bottom: 3em;
-				margin-top: -3em;
-			}
+.box .image.featured:first-child {
+	border-radius: 6px 6px 0 0;
+	margin-bottom: 3em;
+	margin-top: -3em;
+}
 
-				.box .image.featured:first-child img {
-					border-radius: 6px 6px 0 0;
-				}
+.box .image.featured:first-child img {
+	border-radius: 6px 6px 0 0;
+}
 
-			.box .image.featured:last-child {
-				border-radius: 0 0 6px 6px;
-				margin-bottom: -3em;
-				margin-top: 3em;
-			}
+.box .image.featured:last-child {
+	border-radius: 0 0 6px 6px;
+	margin-bottom: -3em;
+	margin-top: 3em;
+}
 
-				.box .image.featured:last-child img {
-					border-radius: 0 0 6px 6px;
-				}
+.box .image.featured:last-child img {
+	border-radius: 0 0 6px 6px;
+}
 
 /* Icon */
 
-	.icon {
-		text-decoration: none;
-		border-bottom: none;
-		position: relative;
-	}
+.icon {
+	text-decoration: none;
+	border-bottom: none;
+	position: relative;
+}
 
-		.icon:before {
-			-moz-osx-font-smoothing: grayscale;
-			-webkit-font-smoothing: antialiased;
-			display: inline-block;
-			font-style: normal;
-			font-variant: normal;
-			text-rendering: auto;
-			line-height: 1;
-			text-transform: none !important;
-			font-family: 'Font Awesome 5 Free';
-			font-weight: 400;
-		}
+.icon:before {
+	-moz-osx-font-smoothing: grayscale;
+	-webkit-font-smoothing: antialiased;
+	display: inline-block;
+	font-style: normal;
+	font-variant: normal;
+	text-rendering: auto;
+	line-height: 1;
+	text-transform: none !important;
+	font-family: "Font Awesome 5 Free";
+	font-weight: 400;
+}
 
-		.icon > .label {
-			display: none;
-		}
+.icon>.label {
+	display: none;
+}
 
-		.icon:before {
-			line-height: inherit;
-		}
+.icon:before {
+	line-height: inherit;
+}
 
-		.icon.solid:before {
-			font-weight: 900;
-		}
+.icon.solid:before {
+	font-weight: 900;
+}
 
-		.icon.brands:before {
-			font-family: 'Font Awesome 5 Brands';
-		}
+.icon.brands:before {
+	font-family: "Font Awesome 5 Brands";
+}
 
-		.icon.major {
-			background: #666;
-			border-radius: 100%;
-			color: #ffffff;
-			cursor: default;
-			display: inline-block;
-			height: 5.5em;
-			line-height: 5.5em;
-			margin: 0 0 2em 0;
-			width: 5.5em;
-		}
+.icon.major {
+	background: #666;
+	border-radius: 100%;
+	color: #ffffff;
+	cursor: default;
+	display: inline-block;
+	height: 5.5em;
+	line-height: 5.5em;
+	margin: 0 0 2em 0;
+	width: 5.5em;
+}
 
-			.icon.major:before {
-				font-size: 2.5em;
-			}
+.icon.major:before {
+	font-size: 2.5em;
+}
 
-			.icon.major.accent1 {
-				background: #666;
-				color: #ffffff;
-			}
+.icon.major.accent1 {
+	background: #666;
+	color: #ffffff;
+}
 
-			.icon.major.accent2 {
-				background: #e89980;
-				color: #ffffff;
-			}
+.icon.major.accent2 {
+	background: #e89980;
+	color: #ffffff;
+}
 
-			.icon.major.accent3 {
-				background: #7fcdb8;
-				color: #ffffff;
-			}
+.icon.major.accent3 {
+	background: #7fcdb8;
+	color: #ffffff;
+}
 
-			.icon.major.accent4 {
-				background: #90b0ba;
-				color: #ffffff;
-			}
+.icon.major.accent4 {
+	background: #90b0ba;
+	color: #ffffff;
+}
 
-			.icon.major.accent5 {
-				background: #e5cb95;
-				color: #ffffff;
-			}
+.icon.major.accent5 {
+	background: #e5cb95;
+	color: #ffffff;
+}
 
-			/* Button */
+/* Button */
 
-	input[type="submit"],
-	input[type="reset"],
-	input[type="button"],
-	.button {
-		-moz-appearance: none;
-		-webkit-appearance: none;
-		-ms-appearance: none;
-		appearance: none;
-		-moz-transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
-		-webkit-transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
-		-ms-transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
-		transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
-		background-color: #666;
-		border-radius: 6px;
-		border: 0;
-		color: #ffffff;
-		cursor: pointer;
-		display: inline-block;
-		font-weight: 400;
-		height: 3em;
-		line-height: 3em;
-		padding: 0 2em;
-		text-align: center;
-		text-decoration: none;
-		white-space: nowrap;
-	}
+input[type="submit"],
+input[type="reset"],
+input[type="button"],
+.button {
+	-moz-appearance: none;
+	-webkit-appearance: none;
+	-ms-appearance: none;
+	appearance: none;
+	-moz-transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+	-webkit-transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+	-ms-transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+	transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
+	background-color: #666;
+	border-radius: 6px;
+	border: 0;
+	color: #ffffff;
+	cursor: pointer;
+	display: inline-block;
+	font-weight: 400;
+	height: 3em;
+	line-height: 3em;
+	padding: 0 2em;
+	text-align: center;
+	text-decoration: none;
+	white-space: nowrap;
+}
 
-		input[type="submit"]:hover,
-		input[type="reset"]:hover,
-		input[type="button"]:hover,
-		.button:hover {
-			background-color: #737373;
-		}
+input[type="submit"]:hover,
+input[type="reset"]:hover,
+input[type="button"]:hover,
+.button:hover {
+	background-color: #737373;
+}
 
-		input[type="submit"]:active,
-		input[type="reset"]:active,
-		input[type="button"]:active,
-		.button:active {
-			background-color: #595959;
-		}
+input[type="submit"]:active,
+input[type="reset"]:active,
+input[type="button"]:active,
+.button:active {
+	background-color: #595959;
+}
 
-		input[type="submit"].icon,
-		input[type="reset"].icon,
-		input[type="button"].icon,
-		.button.icon {
-			padding-left: 1.35em;
-		}
+input[type="submit"].icon,
+input[type="reset"].icon,
+input[type="button"].icon,
+.button.icon {
+	padding-left: 1.35em;
+}
 
-			input[type="submit"].icon:before,
-			input[type="reset"].icon:before,
-			input[type="button"].icon:before,
-			.button.icon:before {
-				margin-right: 0.5em;
-			}
+input[type="submit"].icon:before,
+input[type="reset"].icon:before,
+input[type="button"].icon:before,
+.button.icon:before {
+	margin-right: 0.5em;
+}
 
-		input[type="submit"].fit,
-		input[type="reset"].fit,
-		input[type="button"].fit,
-		.button.fit {
-			width: 100%;
-		}
+input[type="submit"].fit,
+input[type="reset"].fit,
+input[type="button"].fit,
+.button.fit {
+	width: 100%;
+}
 
-		input[type="submit"].small,
-		input[type="reset"].small,
-		input[type="button"].small,
-		.button.small {
-			font-size: 0.8em;
-			height: 2.7em;
-			line-height: 2.7em;
-		}
+input[type="submit"].small,
+input[type="reset"].small,
+input[type="button"].small,
+.button.small {
+	font-size: 0.8em;
+	height: 2.7em;
+	line-height: 2.7em;
+}
 
-		input[type="submit"].large,
-		input[type="reset"].large,
-		input[type="button"].large,
-		.button.large {
-			font-size: 1.25em;
-			height: 2.7em;
-			line-height: 2.7em;
-		}
+input[type="submit"].large,
+input[type="reset"].large,
+input[type="button"].large,
+.button.large {
+	font-size: 1.25em;
+	height: 2.7em;
+	line-height: 2.7em;
+}
 
-		input[type="submit"].alt,
-		input[type="reset"].alt,
-		input[type="button"].alt,
-		.button.alt {
-			background-color: transparent;
-			box-shadow: inset 0 0 0 2px #e5e5e5;
-			color: #777;
-		}
+input[type="submit"].alt,
+input[type="reset"].alt,
+input[type="button"].alt,
+.button.alt {
+	background-color: transparent;
+	box-shadow: inset 0 0 0 2px #e5e5e5;
+	color: #777;
+}
 
-			input[type="submit"].alt:hover,
-			input[type="reset"].alt:hover,
-			input[type="button"].alt:hover,
-			.button.alt:hover {
-				background-color: #f8f8f8;
-			}
+input[type="submit"].alt:hover,
+input[type="reset"].alt:hover,
+input[type="button"].alt:hover,
+.button.alt:hover {
+	background-color: #f8f8f8;
+}
 
-			input[type="submit"].alt:active,
-			input[type="reset"].alt:active,
-			input[type="button"].alt:active,
-			.button.alt:active {
-				background-color: #f0f0f0;
-			}
+input[type="submit"].alt:active,
+input[type="reset"].alt:active,
+input[type="button"].alt:active,
+.button.alt:active {
+	background-color: #f0f0f0;
+}
 
-			input[type="submit"].alt.icon:before,
-			input[type="reset"].alt.icon:before,
-			input[type="button"].alt.icon:before,
-			.button.alt.icon:before {
-				color: #999;
-			}
+input[type="submit"].alt.icon:before,
+input[type="reset"].alt.icon:before,
+input[type="button"].alt.icon:before,
+.button.alt.icon:before {
+	color: #999;
+}
 
-		input[type="submit"].primary,
-		input[type="reset"].primary,
-		input[type="button"].primary,
-		.button.primary {
-			background-color: #e89980;
-			color: #ffffff !important;
-		}
+input[type="submit"].primary,
+input[type="reset"].primary,
+input[type="button"].primary,
+.button.primary {
+	background-color: #e89980;
+	color: #ffffff !important;
+}
 
-			input[type="submit"].primary:hover,
-			input[type="reset"].primary:hover,
-			input[type="button"].primary:hover,
-			.button.primary:hover {
-				background-color: #ecaa96;
-			}
+input[type="submit"].primary:hover,
+input[type="reset"].primary:hover,
+input[type="button"].primary:hover,
+.button.primary:hover {
+	background-color: #ecaa96;
+}
 
-			input[type="submit"].primary:active,
-			input[type="reset"].primary:active,
-			input[type="button"].primary:active,
-			.button.primary:active {
-				background-color: #e4886a;
-			}
+input[type="submit"].primary:active,
+input[type="reset"].primary:active,
+input[type="button"].primary:active,
+.button.primary:active {
+	background-color: #e4886a;
+}
 
-		input[type="submit"].disabled, input[type="submit"]:disabled,
-		input[type="reset"].disabled,
-		input[type="reset"]:disabled,
-		input[type="button"].disabled,
-		input[type="button"]:disabled,
-		.button.disabled,
-		.button:disabled {
-			background-color: #777 !important;
-			box-shadow: inset 0 -0.15em 0 0 rgba(0, 0, 0, 0.15);
-			color: #f5f5f5 !important;
-			cursor: default;
-			opacity: 0.25;
-		}
+input[type="submit"].disabled,
+input[type="submit"]:disabled,
+input[type="reset"].disabled,
+input[type="reset"]:disabled,
+input[type="button"].disabled,
+input[type="button"]:disabled,
+.button.disabled,
+.button:disabled {
+	background-color: #777 !important;
+	box-shadow: inset 0 -0.15em 0 0 rgba(0, 0, 0, 0.15);
+	color: #f5f5f5 !important;
+	cursor: default;
+	opacity: 0.25;
+}
 </style>
