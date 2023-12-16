866361d7d0a6477b8321dbce00641ff828025ec9
Satapat123 <63201171@kmitl.ac.th>
2023-12-16 15:47:43 +0700

    hover

--- src/views/AccountApp/Account-info.vue
+++ src/views/AccountApp/Account-info.vue
@@ -38,7 +38,7 @@
             >
               <button
                 type="button"
-                class="mt-4 focus:outline-none text-white bg-[#140A4B] hover:bg-[#140a4bc5] focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-orange-700 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                class="mt-4 focus:outline-none text-white bg-[#140A4B] hover:bg-[#140a4bc5] focus:ring-4 focus:ring-[#140a4bc5] font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-orange-700 dark:hover:bg-[#140a4bc5] dark:focus:ring-[#140a4bc5]"
               >
               แก้ไขโปรไฟล์
               </button>
@@ -103,7 +103,7 @@
             <div
               class="flex items-center space-x-2 font-semibold text-gray-900 leading-8"
             >
-              <span clas="text-green-500">
+              <span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
