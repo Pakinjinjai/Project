562205fec7dcb59ce2c1bc2d17d1d9f00d7137f4
PAKIN JINJAI <126375863+Pakinjinjai@users.noreply.github.com>
2024-01-21 14:06:49 +0700

    Update QueueDashboard.vue

--- src/components/QueueDashboard.vue
+++ src/components/QueueDashboard.vue
@@ -128,7 +128,7 @@ export default {
         },
         AddModal(user) {
             this.SelectAddModal = user;
-            console.log("แอดคิวของ :", this.SelectAddModal);
+            console.log("แอดคิวของ :",user.firstname, this.SelectAddModal);
             this.resetModalData();
             this.AddModel = true;
         },
@@ -677,7 +677,7 @@ export default {
                                 <!-- Main modal Layout Queue Add -->
                                 <div id="AddModel" tabindex="-1" aria-hidden="true" :class="{ hidden: !AddModel, flex: AddModel }" class="fixed top-0 right-0 left-0 z-50 justify-center items-center w-full h-full backdrop-contrast-25 bg-black/5">
                                     <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
-                                        
+
                                         <!-- Modal content -->
                                         <div class="relative p-4 bg-white rounded-lg shadow sm:p-5">
 
