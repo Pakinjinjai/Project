38267aafc850cd2907edf647c2576ab8013c5173
PAKIN JINJAI <126375863+Pakinjinjai@users.noreply.github.com>
2024-01-25 06:51:47 +0700

    Update Account-edit.vue

--- src/views/AccountApp/Account-edit.vue
+++ src/views/AccountApp/Account-edit.vue
@@ -129,7 +129,7 @@ export default {
                   </div>
                   <div class="px-4 py-2">
                     <input type="text" name="firstname" id="firstname"
-                      class="bg-gray-100 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full px-2 py-2 text-center"
+                      class="bg-gray-100 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full px-2 py-2"
                       placeholder="ชื่อจริง" required="" v-model="formData.firstname" />
                   </div>
                 </div>
