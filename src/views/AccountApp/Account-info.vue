7a15e4ab5b2852085357451dd6987b14370e48e1
PAKIN JINJAI <126375863+Pakinjinjai@users.noreply.github.com>
2024-01-29 09:56:35 +0700

    DD-MM-YYYY

--- src/views/AccountApp/Account-info.vue
+++ src/views/AccountApp/Account-info.vue
@@ -200,7 +200,7 @@ export default {
           this.profileData = res.data.user;
           this.profileAddress = res.data.user.address;
           this.birthDate = dayjs(this.profileData.birthdate).format(
-            "YYYY-MM-DD"
+            "DD-MM-YYYY"
           );
           // console.log(this.profileData);
           // console.log(this.profileData.address);
