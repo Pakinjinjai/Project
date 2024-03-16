6e684c09efae73a9dae7b04d6b8096c1201e392e
PAKIN JINJAI <pakin.281244@gmail.com>
2024-03-16 15:07:57 +0700

    Update APIGate.js

--- src/APIGate.js
+++ src/APIGate.js
@@ -2,13 +2,10 @@
 /***** Deploy_KMITL_SERVER *****/ 
 // const baseURL = "http://161.246.127.114"
 /***** Deploy_railway.app_SERVERS *****/
-// const baseURL = "https://ppsp-projectbackend-production-5099.up.railway.app/api/v1" 
-
-
-
+const baseURL = "https://ppsp-projectbackend-production-5099.up.railway.app/api/v1" 
 
 ///Development///
-const baseURL = "http://localhost:3000/api/v1" 
+// const baseURL = "http://localhost:3000/api/v1" 
 
 //USERS_APIGATEWAYS//
 const SIGNIN = "/users/login";
